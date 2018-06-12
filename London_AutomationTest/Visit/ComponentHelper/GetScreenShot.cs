fd370fc5c8282acfd505a1f5732818b61240a70d
GideonOgunleye <gogunleye@ssl247.co.uk>
2018-06-12 10:43:40 +0100

    Deploy Fix

---
+++ London_AutomationTest/Visit/ComponentHelper/GetScreenShot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Visit.Settings;
+
+namespace Visit.ComponentHelper
+{
+    public class GetScreenShot
+    {
+        private IWebDriver driver;
+
+        public GetScreenShot(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public static string Capture(IWebDriver driver,string screenShotName)
+        {
+            ITakesScreenshot ts = (ITakesScreenshot) driver;
+            Screenshot screenshot = ts.GetScreenshot();
+            string path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string finalpath = path.Substring(0, path.LastIndexOf("bin")) + "ErrorScreenshots\\" + screenShotName + DateTime.UtcNow.ToString("yyyy-MM-dd-mm-ss") +
+                               ".Jpeg";
+            string localpath = new Uri(finalpath).LocalPath;
+            screenshot.SaveAsFile(localpath, ScreenshotImageFormat.Jpeg);
+            return localpath;
+        }
+    }
+}
---
+++ London_AutomationTest/Visit/FeatureFiles/Sitecore Login.feature
@@ -0,0 +1,23 @@
+﻿Feature: Sitecore Login
+	As a CMS User
+	I Want To Be Able to Login To The Content Editor CMS
+	So That I Can Create And Edit Content
+
+#@mytag
+Scenario: Login With Valid Credentials
+	Given User is On CMS Login Page 'http://qa.cms.londonandpartners.com/sitecore/login'
+	When User Enters Valid Credebtials
+	| Username | Password |
+	| gogunleye   | z  |
+	And Clicks Login Button
+	Then Sitecore Experience Platform Page Should Be Displayed
+	When User Clicks on Content Editor Tab
+	Then Content Editor Page Should Be Displayed
+
+Scenario: Login WIth Invalid Credentials
+	Given User is On CMS Login Page "http://qa.cms.londonandpartners.com/sitecore/login"
+	When User Enters InValid Credebtials
+	| Username | Password |
+	| Valid    | InValid  |
+	And Clicks Login Button
+	Then User Should See Invalid Login Message "" on Screen
